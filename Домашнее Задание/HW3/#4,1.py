fd8f64bad6dbd9bc7e4c8fd51f37d0069f3dc3c0
Konstantin <95898160+Kypik@users.noreply.github.com>
2023-10-15 13:16:05 +0800

    Add files via upload

---
+++ Домашнее Задание/HW3/#4,1.py
@@ -0,0 +1,9 @@
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+if a <= b and a <= c and a <= d:
+    print(a)
+if  b <= a and b <= c and b <= d:
+    print(b)
+if c <= a and c <= b and c <= d:
+    print(c)
+if d <= a and d <= b and d <= c:
+    print(d)
---
+++ Домашнее Задание/HW3/#4,2.py
@@ -0,0 +1,8 @@
+a, b, c, a1, b1, c1 = int(input()), int(input()), int(input()), 0, 0, 0
+if a >= 0:
+    a1 = a
+if b >= 0:
+    b1 = b
+if c >= 0:
+    c1 = c
+print(a1 + b1 + c1)
